4a53ed06aaa2e9c7380c918f5eb9689075d4bc35
FronzyWest <fronzydjsb2@gmail.com>
2022-04-10 01:16:18 +0800

    Created a new model called vehicls

---
+++ mainAPI/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class vehicls(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
